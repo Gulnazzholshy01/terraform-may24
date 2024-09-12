d4c02080ddf75049e994bcf2de40f1db485d7a5a
Gulnazzholshy01 <gulnaz.zholshy@yandex.kz>
2024-09-12 17:10:20 -0500

    private

--- hometasks/session-4/main.tf
+++ hometasks/session-4/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public" {
   )
 }
 
-########## AWS PUBLIC SUBNETS ######
+########## AWS PRIVATE SUBNETS ######
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidr)
   vpc_id            = aws_vpc.main.id
@@ -72,3 +72,51 @@ resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
+
+
+########## EIP ######
+resource "aws_eip" "lb" {
+  domain = "vpc"
+}
+
+
+########## NGW ######
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.lb.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    {Name = format(local.name, "nat-gw")},local.common_tags
+  )
+}
+
+
+########## PRIVATE ROUTE TABLES######
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+  route {
+    cidr_block = var.vpc_cidr
+    gateway_id = "local"
+  }
+
+  tags = merge(
+    { Name = format(local.name, "private-rt") }, local.common_tags
+  )
+}
+
+
+########## PRIVATE ROUTE TABLE  ASSOCIATION ######
+resource "aws_route_table_association" "private_rt_association" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+
+
